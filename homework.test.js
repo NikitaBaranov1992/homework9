e6ec3330fc21822a61b48271821c30ded70844be
NikitaBaranov1992 <2486010@mail.ru>
2023-05-05 22:14:57 +0500

    homework 10.1

--- homework.test.js
+++ homework.test.js
@@ -1,10 +1,10 @@
 const calculateBonus = require("./homework");
 describe("Тестирование функции calculateBonus", () => {
   it("Проверка, если sum меньше 50", () => {
-    expect(calculateBonus(1, 20)).toBe(21);
+    expect(calculateBonus(1, 48)).toBe(49);
   });
   it("Проверка, если sum больше 50", () => {
-    expect(calculateBonus(5, 300)).toBe(50);
+    expect(calculateBonus(1, 50)).toBe(51);
   });
   it("Проверка, если sum равно 50", () => {
     expect(calculateBonus(5, 45)).toBe(50);
